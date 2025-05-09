ebb6c3306c7ada57ad4cc3afc60543fa5743dc1f
Korn <sirichupong2546@gmail.com>
2025-05-09 16:10:06 +0700

    ADD: data_loader, data_filtered func & sidebar setting

--- visualization/app.py
+++ visualization/app.py
@@ -1,8 +1,13 @@
 import os
 import pandas as pd
+import numpy as np
+import plotly.express as px
+import matplotlib.pyplot as plt
 import streamlit as st
 import pyarrow.parquet as pq
 import s3fs
+from datetime import timedelta, datetime
+
 
 # ตั้งค่าการเชื่อมต่อกับ LakeFS
 lakefs_endpoint = os.getenv("LAKEFS_ENDPOINT", "http://lakefs-dev:8000")
@@ -16,16 +21,82 @@ fs = s3fs.S3FileSystem(
     client_kwargs={'endpoint_url': lakefs_endpoint}
 )
 
-# ตำแหน่งไฟล์ Parquet ใน LakeFS
-lakefs_s3_path = 's3a://air-quality/main/airquality.parquet/year=2025/month=5/day=4/hour=2/7400decd6afd4d1caa00b19993ed2e7a-0.parquet'
-
 @st.cache_data
 def load_data():
-    # โหลดไฟล์ Parquet จาก LakeFS
-    parquet_file = pq.ParquetFile(fs.open(lakefs_s3_path))
-    df = parquet_file.read().to_pandas()
-    return df
+    lakefs_path = "s3://air-quality/main/airquality.parquet/year=2025"
+    data_list = fs.glob(f"{lakefs_path}/*/*/*/*")
+    df_all = pd.concat([pd.read_parquet(f"s3://{path}", filesystem=fs) for path in data_list], ignore_index=True)
+    df_all['lat'] = pd.to_numeric(df_all['lat'], errors='coerce')
+    df_all['long'] = pd.to_numeric(df_all['long'], errors='coerce')
+    df_all['year'] = df_all['year'].astype(int) 
+    df_all['month'] = df_all['month'].astype(int)
+    df_all.drop_duplicates(inplace=True)
+    df_all['PM25.aqi'] = df_all['PM25.aqi'].mask(df_all['PM25.aqi'] < 0, pd.NA)
+    # เติมค่าจากค่า "ก่อนหน้า" โดยกลุ่มตาม stationID
+    df_all['PM25.aqi'] = df_all.groupby('stationID')['PM25.aqi'].transform(lambda x: x.fillna(method='ffill'))
+    return df_all
 
+def filter_data(df, start_date, end_date, station):
+    df_filtered = df.copy()
+
+    # Filter by date
+    df_filtered = df_filtered[
+        (df_filtered['timestamp'].dt.date >= start_date) &
+        (df_filtered['timestamp'].dt.date <= end_date)
+    ]
+
+    # Filter by station
+    if station != "ทั้งหมด":
+        df_filtered = df_filtered[df_filtered['nameTH'] == station]
+
+    # Remove invalid AQI
+    df_filtered = df_filtered[df_filtered['PM25.aqi'] >= 0]
+
+    return df_filtered
+
+
+st.set_page_config(
+    page_title = 'Real-Time Air Quality Dashboard',
+    # page_icon = '✅',
+    layout = 'wide'
+)
 st.title("Air Quality Dashboard from LakeFS")
 df = load_data()
-st.write(df.head())
+#col = st.columns((1.5, 4.5, 2), gap='medium')
+
+
+# Set up input widgets
+# st.logo(image="images/streamlit-logo-primary-colormark-lighttext.png", 
+#         icon_image="images/streamlit-mark-color.png")
+
+# Sidebar settings
+with st.sidebar:
+    st.title("Air4Thai Dashboard")
+    st.header("⚙️ Settings")
+
+    max_date = df['timestamp'].max().date()
+    min_date = df['timestamp'].min().date()
+    default_start_date = min_date
+    default_end_date = max_date
+
+    start_date = st.date_input(
+        "Start date",
+        default_start_date,
+        min_value=min_date,
+        max_value=max_date
+    )
+
+    end_date = st.date_input(
+        "End date",
+        default_end_date,
+        min_value=min_date,
+        max_value=max_date
+    )
+
+    station_name = df['nameTH'].dropna().unique().tolist()
+    station_name.sort()
+    station_name.insert(0, "ทั้งหมด")
+    station = st.selectbox("Select Station", station_name)
+
+
+
